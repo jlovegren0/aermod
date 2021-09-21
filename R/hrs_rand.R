f902bf7948efbfe2fa3faff9ed7fdd3ecc94506b
jlovegren0 <jlovegren@gmail.com>
2021-09-20 21:49:03 -0500

    add emission scalar option

--- R/hrs_rand.R
+++ R/hrs_rand.R
@@ -12,7 +12,14 @@
 #' @param which_filter Integer specifying special filters on the random number generation. Default is 1 (no filter). 2 is case where nblock = 1 and blocks in successive years must be spaced at least 365 days apart.
 #' @return An integer vector of operating hours, which may be used to index rows in an impact matrix.
 #' @export
-hrs_rand <- function(yrspan=2016L:2020L,nblock=6L,blocklen=1L,which_filter=1L,units=c("hours","days")){
+hrs_rand <- function(yrspan=2016L:2020L,
+        nblock=6L,
+        blocklen=1L,
+        which_filter=1L,
+        units=c("hours","days"),
+        emis_scale = 1)
+{   
+    if ( all( length(emis_scale) != c(1L,nblock) ) ){ stop('Error: Bad emission scalar length (need 1 or nblock)') }
     filters <- list( function(x){TRUE}, function(x) all( diff(x) > 365L*24L ) )
     if( blocklen > 1L ){ which_filter=1L }
     dyy <- purrr::map_int( yrspan , ~ dplyr::case_when( lubridate::leap_year(.) ~ 366L , TRUE ~ 365L ) )
@@ -33,7 +40,14 @@ hrs_rand <- function(yrspan=2016L:2020L,nblock=6L,blocklen=1L,which_filter=1L,un
         start_hrs <- purrr::map( seq_along(yrspan), ~ L(hry[.]) + offs_hr[.] ) %>% unlist 
         if ( filters[[which_filter]](start_hrs) ){ break }
     }
-        purrr::map( start_hrs, ~ .:(.+(blocklen-1L)) ) %>% unlist %>%
-        purrr::map( function(.x) ((.x-1)*anti_mag+1):(.x*anti_mag) ) %>% unlist %>% sort
-
+    hrlist <- purrr::map( start_hrs, ~ .:(.+(blocklen-1L)) ) %>% unlist %>%
+        purrr::map( function(.x) ((.x-1)*anti_mag+1):(.x*anti_mag) ) %>% unlist 
+    if ( length(emis_scale) > 1 )
+    {
+    emscal <- unlist(purrr::map(1:length(yrspan),~sample(emis_scale)))
+    elist <- purrr::map( emscal, ~ rep(.,blocklen)) %>% unlist %>%
+        purrr::map( ~rep(.,anti_mag) ) %>% unlist 
+    }
+    else elist <- emis_scale
+    tibble(hrlist,elist)
 }
