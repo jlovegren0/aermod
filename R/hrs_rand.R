920a038503d351d6b69fa6d620356fb231bd2751
jlovegren0 <jlovegren@gmail.com>
2021-09-20 20:26:44 -0500

    units option for hrs_rand

--- R/hrs_rand.R
+++ R/hrs_rand.R
@@ -8,15 +8,18 @@
 #' @param yrspan Calendar years for which operating hours are to be generated. A numeric vector
 #' @param nblock Number of blocks of consecutive hours. 
 #' @param blocklen Number of hours in each block. Default is 1.
+#' @param units for \code{blocklen}, either \code{"hours"} (default) or \code{"days"}. Return value is always vector of hours.
 #' @param which_filter Integer specifying special filters on the random number generation. Default is 1 (no filter). 2 is case where nblock = 1 and blocks in successive years must be spaced at least 365 days apart.
 #' @return An integer vector of operating hours, which may be used to index rows in an impact matrix.
 #' @export
-hrs_rand <- function(yrspan=2016L:2020L,nblock=6L,blocklen=1L,which_filter=1L){
+hrs_rand <- function(yrspan=2016L:2020L,nblock=6L,blocklen=1L,which_filter=1L,units=c("hours","days")){
     filters <- list( function(x){TRUE}, function(x) all( diff(x) > 365L*24L ) )
     if( blocklen > 1L ){ which_filter=1L }
-    hry <- `*`(24L,purrr::map_int( yrspan , 
-        ~ dplyr::case_when( lubridate::leap_year(.) ~ 366L , TRUE ~ 365L ) ))
-    offs <- c(0,cumsum(hry)[1:(length(hry)-1)])
+    dyy <- purrr::map_int( yrspan , ~ dplyr::case_when( lubridate::leap_year(.) ~ 366L , TRUE ~ 365L ) )
+    mag <- ifelse(all(units=="days"),1L,24L) 
+    anti_mag <- ifelse(all(units=="days"),24L,1L)
+    hry <- `*`(mag,dyy)
+    offs_hr <- c(0,cumsum(hry)[1:(length(hry)-1)])
     L <- function(N){ 
         draw <- function()  sample(1L:(N-blocklen+1),nblock)  %>% sort
         u <- draw()
@@ -27,8 +30,10 @@ hrs_rand <- function(yrspan=2016L:2020L,nblock=6L,blocklen=1L,which_filter=1L){
     }
     repeat 
     {
-        start_hrs <- purrr::map( seq_along(yrspan), ~ L(hry[.]) + offs[.] ) %>% unlist 
+        start_hrs <- purrr::map( seq_along(yrspan), ~ L(hry[.]) + offs_hr[.] ) %>% unlist 
         if ( filters[[which_filter]](start_hrs) ){ break }
     }
-        purrr::map( start_hrs, ~ .:(.+(blocklen-1L)) ) %>% unlist
+        purrr::map( start_hrs, ~ .:(.+(blocklen-1L)) ) %>% unlist %>%
+        purrr::map( function(.x) ((.x-1)*anti_mag+1):(.x*anti_mag) ) %>% unlist %>% sort
+
 }
