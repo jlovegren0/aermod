656a82bd7f81d00c4eb1c0ba0bca00dc4a6e9a68
jlovegren0 <jlovegren@gmail.com>
2021-09-22 14:30:27 -0500

    add ... to scan_postfile to address big endian system-generated POST files

--- R/scan_postfile.R
+++ R/scan_postfile.R
@@ -10,14 +10,14 @@
 #' @importFrom magrittr %>%
 #' @importFrom magrittr %<>%
 #' @export
-scan_postfile <- function(f){
+scan_postfile <- function(f,...){
 rewind <- function(fp) seek(fp,0)
 fp <- file(f,open="rb",raw=TRUE)
 on.exit(close(fp))
-	reclen <- readBin(fp,what='integer')
-	hr <- readBin(fp,what='integer')
-	aveper <- readBin(fp,what='integer')
-	srcgrp <- readBin(fp,what='raw',n=8) %>% rawToChar
+	reclen <- readBin(fp,what='integer',...)
+	hr <- readBin(fp,what='integer',...)
+	aveper <- readBin(fp,what='integer',...)
+	srcgrp <- readBin(fp,what='raw',n=8,...) %>% rawToChar
 	nc <- (reclen - 16) / 8
 seek(fp,where=0,origin="end")
 nbytes <- seek(fp)
@@ -27,7 +27,7 @@ container <- vector(mode="double",length= nhrs * nc )
 for ( hh in 1:nhrs )
 {
 	seek(fp,where=20,origin="current")
-	container[ ((hh-1) * nc + 1) : (hh*nc) ] <- readBin(fp,what='double',n=nc)
+	container[ ((hh-1) * nc + 1) : (hh*nc) ] <- readBin(fp,what='double',n=nc,...)
 	seek(fp,where=4,origin="current")
 }
 res <- matrix( container , ncol = nc , byrow=TRUE)
